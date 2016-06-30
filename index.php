2fc6ab0eaf59b6a90c43b169b030ba04094c8d6f
lea-vasseur <lea.vasseur@free.fr>
2016-06-30 11:44:12 +0200

    fichier log et gitignore

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ $file = fopen("texte.txt", 'r');
 var_dump(fread($file, filesize("texte.txt")));
 fclose($file);
 
+$file = fopen("texte.log", 'a');
+fwrite($file, date("Y-m-d H:i:s")." : le script a été ouvert\r\n");
+fclose($file);
+
+
 
 ?>
